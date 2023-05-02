ae712de7cafc7ef3a44d6b59a4903bf174731ea8
Gregory0401 <grigoriosdart@gmail.com>
2023-05-02 20:46:49 +0300

    fin

--- src/components/fetch/api.jsx
+++ src/components/fetch/api.jsx
@@ -1,12 +1,9 @@
 import axios from "axios";
 
-const SEARCH_PARAMS = new URLSearchParams({
-  safesearch: "true",
-});
 export async function fetchTitles(page) {
   const BASE_URL = `https://6438413f4660f26eb198c6ce.mockapi.io/users`;
   try {
-    const titles = await axios.get(`${BASE_URL}?${SEARCH_PARAMS}`);
+    const titles = await axios.get(`${BASE_URL}`);
     return titles;
   } catch (error) {
     throw new Error(console.log(error));
