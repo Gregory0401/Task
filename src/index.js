722a4e154daa39f93ef433cee23e472b382db0ea
Gregory0401 <grigoriosdart@gmail.com>
2023-05-16 12:56:14 +0300

    final

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/test-task-social">
+    <BrowserRouter basename="/Task">
       <Provider store={store}>
         <App />
       </Provider>
